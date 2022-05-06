a2a5ea6facb4aabb0e719d74c956b19d835b659e
HighSpeed <lawrencerliu@ucla.edu>
2022-05-06 00:29:07 -0700

    Working, BY MYSELF

---
+++ CarLab/lines_utils.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Line(object):
+    def __init__(self,start:tuple,end:tuple):
+        self.startX=start[0]
+        self.startY=start[1]
+        
+        self.endX=end[0]
+        self.endY=end[1]
+        
+        self.slope=(self.endY-self.startY)/(self.endX-self.startX)
+        #print(self.slope)
+    def contains(self,sensor_loc,thresh=10):
+        """
+        determine whether the sensor array will "see" this array
+        sensor array consists of a section of points, X and Y
+        
+        sensor_loc is a dict with 4 values
+        x1,y1,x2,y2 which define the size and location of the sensor array
+        
+        if it does, it returns the location along the array,
+        otherwise returns None
+        """
+        #sensor is a line, calculate its slope
+        sensorSlope=(sensor_loc["y2"]-sensor_loc["y1"])/(sensor_loc["x2"]-sensor_loc["x1"])
+                                                          
+        #find the x intercept between these two lines, 
+        #we have sensorSlope*(x-sensor_loc["x1"])+sensor_loc["y1"]=lineSlope(x-startX)+startY
+                                                         
+                                                         
+        x_intercept=(self.startY-sensor_loc["y1"]-self.startX*self.slope+sensor_loc["x1"]*sensorSlope)/\
+                                                       (sensorSlope-self.slope)
+#         print(x_intercept)
+#         print(sensorSlope*(x_intercept-sensor_loc["x1"])+sensor_loc["y1"])
+#         print(self.slope*(x_intercept-self.startX)+self.startY)
+        #if x_intercept is in the correct range
+        #check if the x_intercept is in the range of this line
+        if x_intercept<max(self.endX,self.startX) and x_intercept>min(self.endX,self.startX):
+            if x_intercept<max(sensor_loc["x2"],sensor_loc["x1"]) and x_intercept>min(sensor_loc["x2"],sensor_loc["x1"]):
+                return x_intercept, sensorSlope*(x_intercept-sensor_loc["x1"])+sensor_loc["y1"]
+        return None
+    
+    def draw(self,ax):
+        #print([self.startX,self.endX])
+        plt.plot([self.startX,self.endX],[self.startY,self.endY],color="black")
+        
+def make_curve(degrees, r, start_point,n_lines,left=True,start_degree=0):
+    """
+    draws the curve as a bunch of lines
+    """
+    if left:
+        d=np.linspace(np.radians(start_degree),np.radians(degrees+start_degree),n_lines+1)
+        X=r*(np.cos(d)-np.cos(np.radians(start_degree)))
+        Y=r*(np.sin(d)-np.sin(np.radians(start_degree)))
+    else:
+        #therefore curve right
+        d=np.linspace(np.pi+np.radians(start_degree),np.radians(180-degrees+start_degree),n_lines+1)
+        X=r*(np.cos(d)-np.cos(np.radians(start_degree+180)))
+        Y=r*(np.sin(d)-np.sin(np.radians(start_degree+180)))
+    #bias to start point
+    X+=start_point[0]
+    Y+=start_point[1]
+    #print(X)
+    
+    #make lines
+    lines=[]
+    for i in range(n_lines):
+        lines.append(Line([X[i],Y[i]],
+                     [X[i+1],Y[i+1]]))
+    return lines
+    
