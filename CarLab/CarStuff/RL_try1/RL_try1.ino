50b8c374214ca51ea2ed4f10e6d51cfd9b79fe1c
HighSpeed <lawrencerliu@ucla.edu>
2022-05-24 19:16:51 -0700

    working

--- CarLab/CarStuff/RL_try1/RL_try1.ino
+++ CarLab/CarStuff/RL_try1/RL_try1.ino
@@ -26,42 +26,79 @@ struct memorySample{
   uint16_t next_sensor_values[8];
 };
 
+void copy_sensor_values(uint16_t* sensor_values,uint16_t* memory_sensor_values){
+  for (int i=0; i<8; i++){
+    memory_sensor_values[i]=sensor_values[i];
+  }
+}
 
 class Memory{
   public:
-    void Memory(){};
+    Memory(){}
 
     bool Add(memory_node newObservation){
       if (observations_taken<=max_observations){
-        memory_node[observations_taken]=newObservation;
+        memory[observations_taken]=newObservation;
         observations_taken++;
         return true;
         }
       return false;
     }
 
-    memorySample sample(){
+    memorySample randomSample(){
       int index=random(observations_taken-1); //choose an observation, cannot be the last observation because it does not have a next value
 
       memorySample sampled;
-      sampled.sensor_values=Memory[index].sensor_values;
-      sampled.action=Memory[index].action;
-      sampled.reward=Memory[index].reward;
-      sampled.next_sensor_values=Memory[index+1].sensor_values;
-      return sampled
+
+
+      copy_sensor_values(memory[index].sensor_values,sampled.sensor_values);
+      sampled.action=memory[index].action;
+      sampled.reward=memory[index].reward;
+      copy_sensor_values(memory[index+1].sensor_values,sampled.sensor_values);
+      return sampled;
    }
 
-     
+   int nObs(){
+    return observations_taken;
+   }
 
+   memory_node getSample(int index){
+    return memory[index];
     
+   } 
   private: 
-    memory_node Memory[5000];
+    memory_node memory[1000];
     int observations_taken=0;
-    int max_observations=5000;
+    int max_observations=1000;
     
 };
 
+//Memory memory;
+memory_node memory[4000];
+int observations_taken=0;
+int max_observations=4000;
 
+bool Add(memory_node newObservation){
+      if (observations_taken<=max_observations){
+        memory[observations_taken]=newObservation;
+        observations_taken++;
+        return true;
+        }
+      return false;
+}
+
+memorySample randomSample(){
+    int index=random(observations_taken-1); //choose an observation, cannot be the last observation because it does not have a next value
+
+    memorySample sampled;
+
+
+    copy_sensor_values(memory[index].sensor_values,sampled.sensor_values);
+    sampled.action=memory[index].action;
+    sampled.reward=memory[index].reward;
+    copy_sensor_values(memory[index+1].sensor_values,sampled.sensor_values);
+    return sampled;
+}
 
 
 
